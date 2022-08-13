8c0674d56e966bc91f9fad3ffee46cfb750e9b66
royalboss <ayushmunot921@gmail.com>
2022-08-13 12:36:16 +0530

    Day93 - 2D Dynamic Programming | Largest Square area in Matrix

---
+++ Day93/01.cpp
@@ -0,0 +1,117 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int solve(vector<vector<int>> &mat, int i, int j, int &maxi)
+    {
+        if (i >= mat.size() || j >= mat[0].size())
+            return 0;
+
+        int right = solve(mat, i, j + 1, maxi);
+        int diag = solve(mat, i + 1, j + 1, maxi);
+        int down = solve(mat, i + 1, j, maxi);
+
+        if (mat[i][j] == 1)
+        {
+            int ans = 1 + min(right, min(diag, down));
+            maxi = max(maxi, ans);
+            return ans;
+        }
+        return 0;
+    }
+
+    int solveMem(vector<vector<int>> &mat, int i, int j, int &maxi, vector<vector<int>> &dp)
+    {
+        if (i >= mat.size() || j >= mat[0].size())
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int right = solveMem(mat, i, j + 1, maxi, dp);
+        int diag = solveMem(mat, i + 1, j + 1, maxi, dp);
+        int down = solveMem(mat, i + 1, j, maxi, dp);
+
+        if (mat[i][j] == 1)
+        {
+            dp[i][j] = 1 + min(right, min(diag, down));
+            maxi = max(maxi, dp[i][j]);
+            return dp[i][j];
+        }
+        return dp[i][j] = 0;
+    }
+
+    int solveTab(vector<vector<int>> &mat, int &maxi)
+    {
+        int row = mat.size();
+        int col = mat[0].size();
+
+        vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+
+        for (int i = row - 1; i >= 0; i--)
+        {
+            for (int j = col - 1; j >= 0; j--)
+            {
+                int right = dp[i][j + 1];
+                int diag = dp[i + 1][j + 1];
+                int down = dp[i + 1][j];
+
+                if (mat[i][j] == 1)
+                {
+                    dp[i][j] = 1 + min(right, min(diag, down));
+                    maxi = max(maxi, dp[i][j]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    int solveFast(vector<vector<int>> &mat, int &maxi)
+    {
+        int row = mat.size();
+        int col = mat[0].size();
+
+        vector<int> curr(col + 1, 0);
+        vector<int> next(col + 1, 0);
+
+        for (int i = row - 1; i >= 0; i--)
+        {
+            for (int j = col - 1; j >= 0; j--)
+            {
+                int right = curr[j + 1];
+                int diag = next[j + 1];
+                int down = next[j];
+
+                if (mat[i][j] == 1)
+                {
+                    curr[j] = 1 + min(right, min(diag, down));
+                    maxi = max(maxi, curr[j]);
+                }
+                else
+                    curr[j] = 0;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+
+    int maxSquare(int n, int m, vector<vector<int>> mat)
+    {
+        int maxi = 0;
+        // solve(mat, 0, 0, maxi);
+
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
+        // solveMem(mat, 0, 0, maxi, dp);
+
+        // solveTab(mat, maxi);
+
+        solveFast(mat, maxi);
+
+        return maxi;
+    }
+};
+
+// https://practice.geeksforgeeks.org/problems/largest-square-formed-in-a-matrix0806/1
